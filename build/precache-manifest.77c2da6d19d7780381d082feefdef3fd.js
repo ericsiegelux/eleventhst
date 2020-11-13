d28fdea500eb5b6bb3f4df4718570aa29232b3c1
Eriq Seagull <ericsiegel448@gmail.com>
2020-11-12 17:36:21 -0800

    build

---
+++ build/precache-manifest.77c2da6d19d7780381d082feefdef3fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6c458b3ed2ba8b52c7f1729c12c7d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b41b8239ca325c888a42",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "d83724af445e1365ea27",
+    "url": "/static/css/main.6482d395.chunk.css"
+  },
+  {
+    "revision": "b41b8239ca325c888a42",
+    "url": "/static/js/2.88c94825.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.88c94825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d83724af445e1365ea27",
+    "url": "/static/js/main.7dba1124.chunk.js"
+  },
+  {
+    "revision": "31a8851c382105e741d9",
+    "url": "/static/js/runtime-main.b8506ae7.js"
+  }
+]);
